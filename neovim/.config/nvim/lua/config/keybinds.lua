741be4bd4629ae22c78a8251b050e0a254ef20aa
Sebastian Cooper-Giannotta <109829617+Mr25300@users.noreply.github.com>
2026-06-25 00:37:59 -0400

    Changed and added window/split keybinds to neovim

--- neovim/.config/nvim/lua/config/keybinds.lua
+++ neovim/.config/nvim/lua/config/keybinds.lua
@@ -3,10 +3,19 @@ vim.g.maplocalleader = " "
 
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex, { desc = "Open file explorer" })
 
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<A-h>", "<C-w><C-h>", { desc = "Focus: Move left" })
+vim.keymap.set("n", "<A-l>", "<C-w><C-l>", { desc = "Focus: Move right" })
+vim.keymap.set("n", "<A-j>", "<C-w><C-j>", { desc = "Focus: Move down" })
+vim.keymap.set("n", "<A-k>", "<C-w><C-k>", { desc = "Focus: Move up" })
+
+vim.keymap.set("n", "<A-v>", "<cmd>vsplit<cr>", { desc = "Split: Create vertical" })
+vim.keymap.set("n", "<A-s>", "<cmd>split<cr>",  { desc = "Split: Create horizontal" })
+vim.keymap.set("n", "<A-c>", "<cmd>close<cr>",  { desc = "Split: Close current" })
+
+vim.keymap.set("n", "<A-Up>", "<cmd>resize +2<CR>", { desc = "Split: Increase height", silent = true })
+vim.keymap.set("n", "<A-Down>", "<cmd>resize -2<CR>", { desc = "Split: Decrease height", silent = true })
+vim.keymap.set("n", "<A-Left>", "<cmd>vertical resize -2<CR>", { desc = "Split: Decrease width", silent = true })
+vim.keymap.set("n", "<A-Right>", "<cmd>vertical resize +2<CR>", { desc = "Split: Increase width", silent = true })
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlights" })
 
